bf8043007e0ed1b2cca15046f968516b6cd628f8
Curovearth <mstrider10@gmail.com>
2022-02-06 21:56:03 +0530

    add files

---
+++ 2-Some Simple Numerical Programs/12-Float-1.py
@@ -0,0 +1,10 @@
+# Float gone MAD ! And I am not joking
+
+x = 0.0
+for i in range(10):
+    x = x+0.1
+    print(x)
+if x ==1.0:
+    print(x,'=1')
+else:
+    print(x,'is not 1.0')
---
+++ 2-Some Simple Numerical Programs/13-NRmethod-squareroot.py
@@ -0,0 +1,10 @@
+# NEWTON RAPHSON method for square root
+# Find x such that x**2-24 is within epsilon of 0
+
+epsilon = 0.01
+k = 24
+guess = k/2.0
+while abs(guess*guess - k) >= epsilon:
+    guess = guess - (((guess**2)-k)/(2*guess))
+    print(guess)
+print('Square root of',k,'is about',guess)
