7dcb66f9ccdf995b3ff5fa191fc17bc2eb6aaadb
Curovearth <mstrider10@gmail.com>
2022-02-13 23:41:10 +0530

    add 10-finger exercise file

--- 3-Functions, Scoping and Abstraction/10-finger-exercise.py
+++ 3-Functions, Scoping and Abstraction/10-finger-exercise.py
@@ -1,27 +1,29 @@
 # When the implementation of fib is used to compute fib(5), how many times does it compute the value of 
 # fib(2) on the way to computing fib(5)
 
-
 def fib(n):
-    if n==0 or n==1:
+    if n==0:
+        return 0
+    elif n==1 or n==2:
         return 1
     else:
+        return fib(n-1) + fib(n-2)
+    
+def testFib(number,toCall):
+    if number==toCall or number-toCall==2:
+        ans=1
+    elif number>toCall:
+        for i in range(number+1):
+            print('fib of',i,'=',fib(i))
+        ans = fib(number-toCall+1)
+    else:
+        ans = 'Not Valid'
+    print("number of times",toCall,"is called =",ans)
 
-        ans = fib(n-1) + fib(n-2)
-        if n==2:
-            return ans
-        else:
-            return ans
-        
+x = int(input("Fibonacci of= "))
+k = int(input("to call= "))
 
-def testFib(n):
-    for i in range(n+1):
-        
-        print('fib of',i,'=',fib(i))
-        
-
-
-testFib(5)
+testFib(x,k)
 
 
 # The fib(k - n + 1) will give number of times fib(n) called when calculating fib(k) recursively, where k > n and this works for n = 0 as well.
