63e8132abab4276b6dab819489f6486ab81a24e8
Curovearth <mstrider10@gmail.com>
2022-02-22 23:09:57 +0530

    add functions as objects

---
+++ 04-Structured types, Mutability, and HOF/10-Functions as objects.py
@@ -0,0 +1,18 @@
+def applyToEach(L,f):
+    for i in range(len(L)):
+        L[i]=f(L[i])
+
+L = [1,-2,3.33]
+print('L=',L)
+print('Apply abs to each element of L')
+applyToEach(L,abs)
+print('L=',L)
+print('Apply int to each element of L')
+applyToEach(L,int)
+print('L=',L)
+# print('Apply factorial to each element of L')
+# applyToEach(L,factR)
+# print('L=',L)
+# print('Apply fibonacci to each element of L')
+# applyToEach(L,fib)
+# print('L=',L)
