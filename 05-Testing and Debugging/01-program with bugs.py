4db2bc050cddad2000344f25296b7c29f3bd7f5d
Curovearth <mstrider10@gmail.com>
2022-02-26 19:03:17 +0530

    add new files

---
+++ 05-Testing and Debugging/01-program with bugs.py
@@ -0,0 +1,26 @@
+from operator import truediv
+
+
+def isPal(x):
+    temp =x
+    # temp = x[:]       # correct way - causes a copy of x to be made
+    # temp.reverse()    # correct way
+    temp.reverse
+    if temp==x:
+        return True
+    else:
+        return False
+
+def silly(n):
+    # result = []           # correct location
+    for i in range(n):
+        result = []     
+        elem = input('enter element: ')
+        result.append(elem)
+    print(result)
+    if isPal(result):
+        print('yes')
+    else:
+        print('no')
+
+silly(2)
