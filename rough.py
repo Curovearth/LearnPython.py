a975187af782c34d6f10b5a4671f281ddf1cf2b5
Curovearth <mstrider10@gmail.com>
2022-03-15 23:25:51 +0530

    rough updation

--- rough.py
+++ rough.py
@@ -1,4 +1,24 @@
-x = -1
+'''
+Pseudo Code
 
-if x < 0:
-  raise Exception("Sorry, no numbers below zero")
+- using itertools to iterate starting from 1
+- we need to find the total divisors
+
+'''
+
+import itertools, math
+
+def num_divisors(n):
+  toEnd = int(math.sqrt(n))
+  result = sum(2 for i in range(1,toEnd+1) if n%i==0)
+  print(result)
+  return result
+
+triangleNumber = 0
+for i in itertools.count(1):
+  triangleNumber += i
+  if num_divisors(triangleNumber) >= 500:
+    print(triangleNumber)
+    break
+  else:
+    continue
--- rough2.py
+++ rough2.py
@@ -1,5 +1,19 @@
-l=[12,2,3,4]
-l2=[3,7,2,89,1]
-final = []
-final.append(l,l2)
-print(final)
+from math import sqrt
+
+def num_divisors(n):
+	end = int(sqrt(n))
+	print('end=',end)
+	# result = sum(2 for i in range(1, end + 1) if n % i == 0)
+	for i in range(1,end+1):
+		print(n,'%',i)
+		if n%i==0:
+			print(2)
+	# if end**2 == n:
+	# 	result -= 1
+	# return 'num is ',n,result
+	# return result
+
+# for i in range(1,10):
+#     print(num_divisors(i))
+
+print(num_divisors(8))
