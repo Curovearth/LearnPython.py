6dffac40f47fcd1e22e1ad78ede6e1e3d5b9fac4
Curovearth <mstrider10@gmail.com>
2022-03-16 22:50:34 +0530

    update rough2

--- rough2.py
+++ rough2.py
@@ -1,19 +1,7 @@
-from math import sqrt
+file = open('ksjffljsk','r')
+listLines = file.readlines()
+totalSum = sum(int(i) for i in listLines)
+print(totalSum)
+print(str(totalSum)[:10])
+print(len(str(totalSum)[:10]))
 
-def num_divisors(n):
-	end = int(sqrt(n))
-	print('end=',end)
-	# result = sum(2 for i in range(1, end + 1) if n % i == 0)
-	for i in range(1,end+1):
-		print(n,'%',i)
-		if n%i==0:
-			print(2)
-	# if end**2 == n:
-	# 	result -= 1
-	# return 'num is ',n,result
-	# return result
-
-# for i in range(1,10):
-#     print(num_divisors(i))
-
-print(num_divisors(8))
