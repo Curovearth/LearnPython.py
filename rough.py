e6d72c5e417f4a980a85a907d7b14c3db5fc906d
Curovearth <mstrider10@gmail.com>
2022-03-19 22:44:51 +0530

    solved 014 project euler

--- rough.py
+++ rough.py
@@ -1,24 +1,5 @@
-'''
-Pseudo Code
+dictionary = {}
+for i in range(10):
+  dictionary[i] = 4
 
-- using itertools to iterate starting from 1
-- we need to find the total divisors
-
-'''
-
-import itertools, math
-
-def num_divisors(n):
-  toEnd = int(math.sqrt(n))
-  result = sum(2 for i in range(1,toEnd+1) if n%i==0)
-  print(result)
-  return result
-
-triangleNumber = 0
-for i in itertools.count(1):
-  triangleNumber += i
-  if num_divisors(triangleNumber) >= 500:
-    print(triangleNumber)
-    break
-  else:
-    continue
+print(dictionary)
