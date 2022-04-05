10dce8e83a17290edf6398dafae184737629a883
Curovearth <mstrider10@gmail.com>
2022-04-05 23:04:39 +0530

    updated person.py with additional comments

--- 07-Classes and Object Oriented Programming/03-person.py
+++ 07-Classes and Object Oriented Programming/03-person.py
@@ -1,50 +1,56 @@
 import datetime
 
 class Person(object):
+
     def __init__(self,name):
-        '''create a person'''
+        # Creating a person
         self.name = name
         try:
-            lastBlank = name.rindex(' ')
+            lastBlank = name.rindex(' ')        # finds the last occurrence of the specified value.
             self.lastName = name[lastBlank+1:]
         except:
             self.lastName = name
         self.birthday = None
 
     def getName(self):
-        '''return self's full name'''
+        # gets the name of the person
         return self.name
-
-    def getLastName(self):
-        '''return self's last name'''
-        return self.lastName
     
+    def getLastName(self):
+        # gets the lastname of the person
+        return self.lastName
+
     def setBirthday(self,birthdate):
-        '''Assumes birthdate is of type datetime.date
-        Sets self's birthday to birthdate'''
+        # sets the birthday of the person
         self.birthday = birthdate
 
     def getAge(self):
-        '''Returns self's current age in days'''
+        # gets the age of the person in days
         if self.birthday == None:
             raise ValueError
-        return (datetime.date.today() - self.birthday).days
+        return (datetime.date.today()-self.birthday).days
 
     def __lt__(self,other):
-        '''Returns True if self precedes other in alphabetical
-        order, and False otherwise. Comparison is based on last names, but if these are the same full names are compared'''
+        '''
+        Returns True if self precedes other in alphabetical order, and False otherwise. Comparison is based on the 
+        last names, bt if these are the same full names are compared.
+        '''
         if self.lastName == other.lastName:
             return self.name < other.name
         return self.lastName < other.lastName
 
     def __str__(self):
-        '''returns self's name'''
         return self.name
 
+# Instantiating the Person Class
 me = Person('Michael Guttag')
 him = Person('Barack Hussein Obama')
 her = Person('Madonna')
+
 print(him.getLastName())
 him.setBirthday(datetime.date(1961,8,4))
 her.setBirthday(datetime.date(1958,8,16))
+
 print(him.getName(),'is',him.getAge(),'days old')
+
+print(him.getLastName())
