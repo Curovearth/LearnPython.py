79e60f464e02f5581159c10f811a432a63883e18
Curovearth <mstrider10@gmail.com>
2022-04-10 19:22:53 +0530

    add solutions folder for PE

---
+++ 00-Project Euler Problems/Solutions/s015.py
@@ -0,0 +1,11 @@
+# Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, 
+# there are exactly 6 routes to the bottom right corner.
+# How many such routes are there through a 20×20 grid?
+
+'''
+____________ REFERENCES ____________
+
+- https://www.robertdickau.com/lattices.html - (Lattice path and catalan)
+- https://brilliant.org/wiki/catalan-numbers/ - (Catalan numbers )
+
+'''
