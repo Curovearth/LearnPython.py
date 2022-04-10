1c56d2581e3a249c4c18b3302dd31f1ae7a48173
Curovearth <mstrider10@gmail.com>
2022-04-10 17:40:29 +0530

    dealing with **kwargs

--- 07-Classes and Object Oriented Programming/Classes Exercises/07-q6.py
+++ 07-Classes and Object Oriented Programming/Classes Exercises/07-q6.py
@@ -1,7 +1,14 @@
 # Write a Python function student_data () which will print the id of a student (student_id). 
 # If the user passes an argument student_name or student_class the function will print the student name and class.
 
-def student_data(student_id,student_name,student_class):
-    print(student_id)
-    print(student_name)
-    print(student_class)
+def student_data(student_id,**kwargs):
+    print(f'Student Id: {student_id}')
+
+    # if 'student_class' or 'student_name' in kwargs:
+    #     print(f"Student class: {kwargs['student_class']}")
+    
+    for i in kwargs:
+        print(f'{str(i)}: {kwargs[i]}')
+
+
+student_data(892984,student_class='TT414',student_name='swarupt')
--- rough.py
+++ rough.py
@@ -1,45 +1,5 @@
-class IntSet(object):
-  def __init__(self):
-    self.vals = []
-  
-  def insert(self,e):
-    if e not in self.vals:
-      self.vals.append(e)
-    
-  def remove(self,e):
-    try:
-      self.vals.remove(e)
-    except:
-      raise ValueError(str(e)+' not found.')
+def total_fruits(**kwargs):
+  print(kwargs, type(kwargs))
 
-  def member(self,e):
-    return e in self.vals
+total_fruits(banana=5,mango=7,apple=8)
 
-  def getMembers(self):
-    return self.vals
-
-  
-  def __str__(self):
-    self.vals.sort()
-    for e in self.vals:
-      return e
-
-
-s = IntSet()
-
-list = [34,21,53,87]
-
-for e in list:
-  s.insert(e)
-
-print(s.member(4))
-print(s.member(34))
-print(s.remove(87))
-print(s.insert(22))
-
-print(s.getMembers())
-
-# print(s.remove(3))
-
-  
-  
