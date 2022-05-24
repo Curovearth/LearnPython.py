4d3ab2c1f210dc7ff8082c0c004cd5aa350dd60e
Curovearth <mstrider10@gmail.com>
2022-05-24 23:45:26 +0530

    class grades

---
+++ 07-Classes and Object Oriented Programming/09-encapsulation and information hiding.py
@@ -0,0 +1,52 @@
+# Contains a class that can be used to keep track of the grades of a collection of students. Instances of class grades
+# are implemented using a list and a dictionary. The list keeps track of the students in the class. The dictionary maps a student's
+# identification number to a list of grades.
+
+class Grades(object):
+    nextId = 0
+
+    def __init__(self):
+        self.students = []
+        self.grades = {}
+        self.isSorted = True
+        
+
+    def getIdNum(self):
+        self.idNum = nextId
+        nextId += 1
+        return self.idNum
+        
+
+    def addStudents(self,student):
+        if student in self.students:
+            raise ValueError('Duplicate Student')
+        self.students.append(student)
+        
+
+        self.grades[student.getIdNum()]=[]
+        self.isSorted = False
+    
+    def addGrade(self,student,grade):
+        try:
+            self.grades[student.getIdNum()].append(grade)
+        except:
+            raise ValueError('Student not in mapping')
+
+    def getGrades(self,student):
+        try:
+            return self.grades[student.getIdNum()][:]
+        except:
+            raise ValueError('Student not in mapping')
+        
+    def getStudents(self):
+        if not self.isSorted:
+            self.students.sort()
+            self.isSorted = True
+        return self.students[:]
+
+p = Grades()
+l = ['boy1','boy2','boy3']
+for i in l:
+    p.addStudents(i)
+
+print(p.getGrades('boy1'))
